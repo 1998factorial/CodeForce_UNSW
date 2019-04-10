7dc8248dc93d52b5eb3a87fdeaea1537901627d6
1998factorial <edward19980505@gmail.com>
2019-04-10 23:27:48 +1000

    fix

--- wk4/E.cpp
+++ wk4/E.cpp
@@ -10,34 +10,34 @@
 #include <map>
 #include <complex>
 using namespace std;
-
-int n;
-int a[100005];
-int dp[100005];
-int main(){
-	cin >> n;
-	for(int i = 0; i < n; i++)cin >> a[i];
-	memset(dp,0,sizeof(dp));
-	dp[0] = 1;
-	int ret = 0;
-	for(int i = 1; i < n; i++){
-		if(a[i] > a[i-1])dp[i] = dp[i-1] + 1;
-		else dp[i] = 1;
-		ret = max(dp[i],ret);
-	}
-	for(int i = 1; i < n; i++){
-		int j = i - dp[i] + 1;
-		if(j > 0){
-			if(i == j)
-				ret = max(ret,dp[j-1]+1);
-			if(j == 1)
-				ret = max(ret,dp[i]+1);
-			else{
-				if(a[j+1]-1 > a[j-1] || a[j-2]+1 < a[j])
-					ret = max(ret,dp[i]+dp[j-1]);
-			}
-		}
-	}
-	cout << ret << endl;
-	return 0;
+const int maxn = 1e5+7;
+int dp1[maxn],dp2[maxn],a[maxn]; 
+//dp1[i] = length of increasing ending at i
+//dp2[i] = length of increasing starting at i
+int main()
+{
+    int n;
+    scanf("%d",&n);a[0]=1e9+7,a[n+1]=1e9+7;
+    for(int i=1;i<=n;i++)
+        scanf("%d",&a[i]);
+    if(n==1)return puts("1"),0;
+    if(n==2)return puts("2"),0;
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        dp1[i] = 1;
+        if(a[i]>a[i-1])dp1[i]=dp1[i-1]+1;
+        ans = max(ans,dp1[i]);
+    }
+    for(int i=n;i>=1;i--){
+        dp2[i]=1;
+        if(a[i]<a[i+1])dp2[i]=dp2[i+1]+1;
+        ans=max(ans,dp2[i]);
+    }
+    for(int i=1;i<=n;i++){
+        if(a[i-1]<a[i+1]-1)
+            ans=max(ans,dp1[i-1]+dp2[i+1]+1);
+    }
+    for(int i=2;i<=n;i++)ans=max(ans,dp2[i]+1); //might miss case
+    for(int i=1;i<n;i++)ans=max(ans,dp1[i]+1); 
+    cout<<ans<<endl;
 }
